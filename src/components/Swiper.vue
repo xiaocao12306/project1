799798d95e9d9fd21537ce0d964d7cc53a13de13
小曹 <3027478654@qq.com>
2022-06-28 22:44:32 +0800

    1

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-<van-swipe style="background:white" class="my-swipe" :autoplay="3000" indicator-color="white">
-      <van-swipe-item>
+<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item style="padding:0">
         <van-image :src="banner" mode="cover"/>
       </van-swipe-item>
     </van-swipe>
